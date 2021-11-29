7da9d25f61a9ae16efeccb4270d7607f2268443f
jeff <jeff@jeffbryner.com>
2021-11-28 18:14:28 -0800

    add secrets for bot

--- code/variables.tf
+++ code/variables.tf
@@ -7,11 +7,23 @@ variable "project_id" {
 variable "function_name" {
   description = "The name of the function"
   type        = string
-  default     = "function"
+  default     = "slackbot"
 }
 
 variable "default_region" {
   description = "Default region to create resources where applicable."
   type        = string
   default     = "us-central1"
+}
+
+variable "slack_bot_token" {
+  description = "The slackbot token (starts with xoxb-)"
+  type        = string
+  sensitive   = true
+}
+
+variable "slack_signing_secret" {
+  description = "The signing secret for your bot (app credentials page of your bot)"
+  type        = string
+  sensitive   = true
 }
