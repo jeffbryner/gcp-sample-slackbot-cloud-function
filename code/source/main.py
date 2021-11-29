dd36ace7657ec58f80049ccf9181d1b42e7898db
jeff <jeff@jeffbryner.com>
2021-11-28 18:14:59 -0800

    initial bot code

--- code/source/main.py
+++ code/source/main.py
@@ -1,6 +1,70 @@
-import numpy as np
-import pandas as pd
+import logging
+import os
+from slack_bolt import App
+
+# Flask adapter
+from slack_bolt.adapter.flask import SlackRequestHandler
+
+logging.basicConfig(level=logging.DEBUG)
+
+# process_before_response must be True when running on FaaS
+app = App(
+    process_before_response=True,
+    token=os.environ.get("SLACK_BOT_TOKEN"),
+    signing_secret=os.environ.get("SLACK_SIGNING_SECRET"),
+)
+
+# Listens to incoming messages that contain "hello"
+# To learn available listener arguments,
+# visit https://slack.dev/bolt-python/api-docs/slack_bolt/kwargs_injection/args.html
+@app.message("hello")
+def message_hello(message, say):
+    # say() sends a message to the channel where the event was triggered
+    # say(f"Hey there <@{message['user']}>!")
+    say(
+        blocks=[
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": f".. <@{message['user']}>, you get a button!",
+                },
+                "accessory": {
+                    "type": "button",
+                    "text": {"type": "plain_text", "text": "Click Me"},
+                    "action_id": "button_click",
+                },
+            }
+        ],
+        text=f".. <@{message['user']}> you get a button!",
+    )
 
 
-def hello_world(request):
-    return f"Hello World, brought to you by numpy version {np.__version__} and pandas version {pd.__version__}"
+@app.action("button_click")
+def action_button_click(body, ack, say):
+    # Acknowledge the action
+    ack()
+    say(f"<@{body['user']['id']}> clicked the button")
+
+
+@app.event("app_mention")
+def event_test(body, say, logger):
+    logger.info(body)
+    say("Hi from Google Cloud Functions!")
+
+
+handler = SlackRequestHandler(app)
+
+# Cloud Function entrypoint
+def hello_slackbot(request):
+    """HTTP Cloud Function.
+    Args:
+        request (flask.Request): The request object.
+        <https://flask.palletsprojects.com/en/1.1.x/api/#incoming-request-data>
+    Returns:
+        The response text, or any set of values that can be turned into a
+        Response object using `make_response`
+        <https://flask.palletsprojects.com/en/1.1.x/api/#flask.make_response>.
+    """
+    return handler.handle(request)
+
